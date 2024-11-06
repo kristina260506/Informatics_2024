eb75e071dd320401071fa6f6f3a484b6568a1528
unknown <kristinakrukova2006@mail.ru>
2024-11-06 21:46:02 +0300

    выволнила 6 лабораторную

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,10 @@
-package main
+package main 
+ 
+import ("fmt"
+"isuct.ru/informatics2022/lab6"
+)
 
-import "fmt"
-
-func main() {
-	fmt.Println("Hello world")
+func main() { 
+    lab6.RunLab6()
+    fmt.Println("Krukova Kristina Ivanovna") 
 }
